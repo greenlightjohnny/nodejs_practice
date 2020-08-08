c25cbfe73b782028f85c5fd4567374107f7ba84a
fj <castelloiv@gmail.com>
2020-08-08 10:14:12 -0400

    23 minutes

---
+++ index.js
@@ -0,0 +1,4 @@
+const Person = require("./person.js");
+
+const person1 = new Person("Tim", 99);
+person1.greeting();
---
+++ person.js
@@ -0,0 +1,12 @@
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  greeting() {
+    console.log(`Hello ${this.name}, you are ${this.age} years old`);
+  }
+}
+
+module.exports = Person;
