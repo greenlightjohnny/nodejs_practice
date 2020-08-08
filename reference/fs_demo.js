c0705668d2c60a84366e4f396baf00b07d0b810b
fj <castelloiv@gmail.com>
2020-08-08 12:23:06 -0400

    FS module work

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -9,7 +9,45 @@ const path = require("path");
 //fs.mkdir takes in the file name, options, and a callback function.
 
 //Create and write to file
-fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "Helloooo", (err) => {
+fs.writeFile(
+  path.join(__dirname, "/test", "hello.txt"),
+  "Helloooo2222",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
+  }
+);
+
+// Create and append to file
+fs.appendFile(
+  path.join(__dirname, "/test", "hello2.txt"),
+  "Helloooo",
+  (err) => {
+    if (err) throw err;
+    console.log("File written to...");
+    fs.writeFile(
+      path.join(__dirname, "/test", "hello2.txt"),
+      "again",
+      (err) => {
+        if (err) throw err;
+        console.log("File written to...");
+      }
+    );
+  }
+);
+
+//Read file
+fs.readFile(path.join(__dirname, "/test", "hello.txt"), "utf8", (err, data) => {
   if (err) throw err;
-  console.log("File written to...");
+  console.log(data);
 });
+
+//Rename file
+fs.rename(
+  path.join(__dirname, "/test", "hello.txt"),
+  path.join(__dirname, "/test", "hello7.txt"),
+  (err) => {
+    if (err) throw err;
+    console.log("##", "File renamed");
+  }
+);
