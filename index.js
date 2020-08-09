281dbf69dda762a0c72ec900954f0fdaa89dd853
fj <castelloiv@gmail.com>
2020-08-09 10:59:49 -0400

    Testing server 404 res

--- index.js
+++ index.js
@@ -5,39 +5,95 @@ const fs = require("fs");
 //Creating the server
 const server = http.createServer((req, res) => {
   ///Check to see if the / homepage was where the request came from
-  if (req.url === "/") {
-    ///readFile asynchronously reads the entire contents of a file. Takes in a file path, options, and a callback function. The callback function takes in (err, data). Err is an error, and data is the contents of the file. Does not have to be called data, in this case it is labeled content
-    fs.readFile(
-      path.join(__dirname, "public", "index.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
-      }
-    );
+  //   if (req.url === "/") {
+  //     ///readFile asynchronously reads the entire contents of a file. Takes in a file path, options, and a callback function. The callback function takes in (err, data). Err is an error, and data is the contents of the file. Does not have to be called data, in this case it is labeled content
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "index.html"),
+  //       (err, content) => {
+  //         if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" });
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+
+  //   ////Check to see if the request was from the about page
+  //   if (req.url === "/about") {
+  //     console.log("%%%%%%%%%%%");
+  //     fs.readFile(
+  //       path.join(__dirname, "public", "about.html"),
+  //       (err, content) => {
+  //         if (err) throw err;
+  //         res.writeHead(200, { "Content-Type": "text/html" });
+  //         res.end(content);
+  //       }
+  //     );
+  //   }
+
+  //   if (req.url === "/api/user") {
+  //     const users = [
+  //       { name: "bob", age: 40 },
+  //       { name: "John", age: 30 },
+  //     ];
+  //     res.writeHead(200, { "Content-Type": "application/json" });
+  //     res.end(JSON.stringify(users));
+  //   }
+
+  // Build file path
+  let filePath = path.join(
+    __dirname,
+    "public",
+    req.url === "/" ? "index.html" : req.url
+  );
+
+  // Extension of file
+  let extname = path.extname(filePath);
+
+  // Initial contact type
+  let contentType = "text/html";
+
+  // Change contact type based on the ext name from the file path
+  switch (extname) {
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".jpg":
+      contentType = "image/jpg";
+      break;
   }
 
-  ////Check to see if the request was from the about page
-  if (req.url === "/about") {
-    console.log("%%%%%%%%%%%");
-    fs.readFile(
-      path.join(__dirname, "public", "about.html"),
-      (err, content) => {
-        if (err) throw err;
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(content);
+  /// Read File
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if ((err.code = "EN0ENT")) {
+        //// page not found, error that EN0NET means
+        fs.readFile(
+          path.join(__dirname, "public", "404.html"),
+          (err, content) => {
+            res.writeHead(200, { "Content=Type": "text/html" });
+            res.end(content, "utf8");
+          }
+        );
+      } else {
+        /// Some server error
+        res.writeHead(500);
+        res.end(`Server Error: ${err.code}`);
       }
-    );
-  }
-
-  if (req.url === "/api/user") {
-    const users = [
-      { name: "bob", age: 40 },
-      { name: "John", age: 30 },
-    ];
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.end(JSON.stringify(users));
-  }
+    } else {
+      //Success
+      res.writeHead(200, { "Content-Type": contentType });
+      res.end(content, "utf8");
+    }
+  });
 });
 
 /// First checks for the port number in the environment variable, if none is found, uses port 5000.
