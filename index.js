aedb81e6de202b78c1d6207fa0af9f1f1c32ffe5
fj <castelloiv@gmail.com>
2020-08-09 12:22:32 -0400

    git ignore added

--- index.js
+++ index.js
@@ -43,13 +43,9 @@ const server = http.createServer((req, res) => {
   let filePath = path.join(
     __dirname,
     "public",
-    req.url === "/" ? "index.html" : req.url + ".html"
+    req.url === "/" ? "index.html" : req.url
   );
-  console.log(
-    "$$$$$$$$",
 
-    req.url
-  );
   // Extension of file
   let extname = path.extname(filePath);
   console.log("$$$$ext", extname);
